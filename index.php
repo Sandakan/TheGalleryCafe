55798d89d1c8bbf7402cae2ec94d8c7a206d0535
Sandakan Nipunajith <45460443+Sandakan@users.noreply.github.com>
2024-07-24 00:30:55 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Document</title>
+	<link rel="stylesheet" href="public/styles/styles.css">
+	<link rel="stylesheet" href="public/styles/fonts.css">
+	<link rel="shortcut icon" href="public/images/logo.png" type="image/x-icon">
+</head>
+
+<body>
+	<nav>
+		<div class="logo-container">
+			<img src="public/images/logo.png" alt="logo" />
+			<h1>The Gallery Café</h1>
+		</div>
+
+
+		<ul class="nav-links">
+			<li class="active"><a href="./">Home</a></li>
+			<li><a href="#menu">Menu</a></li>
+			<li><a href="#about">About</a></li>
+			<li><a href="#contact">Contact</a></li>
+			<li><a href="#cart"><span class="material-symbols-rounded">
+						shopping_cart
+					</span></a></li>
+			<li><a href="#account"><span class="material-symbols-rounded material-symbols-rounded-filled">
+						account_circle
+					</span></a></li>
+
+		</ul>
+	</nav>
+	<div class="hero">
+		<div class="hero-image-container"></div>
+
+		<div class="hero-text-container">
+			<img src="public/images/logo.png" alt="logo" />
+			<h1>A gallery of <br> memorable taste</h1>
+			<p>A place where friends gather, families celebrate, and visitors get a taste of Sri Lanka's rich cultural heritage.</p>
+		</div>
+
+	</div>
+
+	<footer>
+		<div class="footer-info-container">
+			<p class="info-text">Nestled in the heart of Colombo, The Gallery Café has a rich history that blends culinary excellence with artistic charm. The café was founded in the early 2000s, dreamed of creating a unique dining experience that showcases traditional Sri Lankan cuisine.</p>
+
+			<div>
+				<img src="public/images/logo.png" alt="logo" />
+				<p class="copyright-text">© 2024 The Gallery Café. All rights reserved.</p>
+			</div>
+		</div>
+		<div class="footer-links-container">
+			<div class="link-block">
+				<h4>Support</h4>
+				<ul>
+					<li><a href="#">FAQs</a></li>
+					<li><a href="#">Contact Us</a></li>
+					<li><a href="#">Locate</a></li>
+					<li><a href="#">Accessibility</a></li>
+				</ul>
+			</div>
+			<div class="link-block">
+				<h4>Support</h4>
+				<ul>
+					<li><a href="#">FAQs</a></li>
+					<li><a href="#">Contact Us</a></li>
+					<li><a href="#">Locate</a></li>
+					<li><a href="#">Accessibility</a></li>
+				</ul>
+			</div>
+			<div class="link-block">
+				<h4>Support</h4>
+				<li><a href="#">FAQs</a></li>
+				<li><a href="#">Contact Us</a></li>
+				<li><a href="#">Locate</a></li>
+				<li><a href="#">Accessibility</a></li>
+				</ul>
+			</div>
+			<div class="link-block">
+				<h4>Support</h4>
+				<ul>
+					<li><a href="#">FAQs</a></li>
+					<li><a href="#">Contact Us</a></li>
+					<li><a href="#">Locate</a></li>
+					<li><a href="#">Accessibility</a></li>
+				</ul>
+			</div>
+		</div>
+	</footer>
+</body>
+
+</html>
---
+++ readme.md
@@ -0,0 +1,7 @@
+# The Gallery Café Website
+
+A dynamic, database-driven web application designed to enhance customer experience for The Gallery Café in Colombo.
+Features include an attractive and user-friendly interface, online table reservations, pre-ordering capabilities,
+detailed menus, high-quality images, and user account management for admins, operational staff, and customers. Developed
+using modern web technologies for both front-end and back-end functionalities, ensuring a seamless and engaging
+experience for all users.

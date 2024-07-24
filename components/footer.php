087875a0b035a66d068946874c03d65479ea5253
Sandakan Nipunajith <45460443+Sandakan@users.noreply.github.com>
2024-07-24 08:52:31 +0530

    Moved nav and footer to php components

---
+++ components/footer.php
@@ -0,0 +1,52 @@
+<?php
+echo <<< HTML
+
+    <footer>
+    <div class="footer-info-container">
+        <p class="info-text">Nestled in the heart of Colombo, The Gallery Café has a rich history that blends culinary excellence with artistic charm. The café was founded in the early 2000s, dreamed of creating a unique dining experience that showcases traditional Sri Lankan cuisine.</p>
+
+        <div>
+            <img src="public/images/logo.png" alt="logo" />
+            <p class="copyright-text">© 2024 The Gallery Café. All rights reserved.</p>
+        </div>
+    </div>
+    <div class="footer-links-container">
+        <div class="link-block">
+            <h4>Support</h4>
+            <ul>
+                <li><a href="#">FAQs</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Locate</a></li>
+                <li><a href="#">Accessibility</a></li>
+            </ul>
+        </div>
+        <div class="link-block">
+            <h4>Support</h4>
+            <ul>
+                <li><a href="#">FAQs</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Locate</a></li>
+                <li><a href="#">Accessibility</a></li>
+            </ul>
+        </div>
+        <div class="link-block">
+            <h4>Support</h4>
+            <li><a href="#">FAQs</a></li>
+            <li><a href="#">Contact Us</a></li>
+            <li><a href="#">Locate</a></li>
+            <li><a href="#">Accessibility</a></li>
+            </ul>
+        </div>
+        <div class="link-block">
+            <h4>Support</h4>
+            <ul>
+                <li><a href="#">FAQs</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Locate</a></li>
+                <li><a href="#">Accessibility</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
+
+HTML;
--- index.php
+++ index.php
@@ -11,27 +11,8 @@
 </head>
 
 <body>
-	<nav>
-		<div class="logo-container">
-			<img src="public/images/logo.png" alt="logo" />
-			<h1>The Gallery Café</h1>
-		</div>
+	<?php include('components/header_navigation_bar.php'); ?>
 
-
-		<ul class="nav-links">
-			<li class="active"><a href="./">Home</a></li>
-			<li><a href="#menu">Menu</a></li>
-			<li><a href="#about">About</a></li>
-			<li><a href="#contact">Contact</a></li>
-			<li><a href="#cart"><span class="material-symbols-rounded">
-						shopping_cart
-					</span></a></li>
-			<li><a href="#account"><span class="material-symbols-rounded material-symbols-rounded-filled">
-						account_circle
-					</span></a></li>
-
-		</ul>
-	</nav>
 	<div class="hero">
 		<div class="hero-image-container"></div>
 
@@ -43,53 +24,7 @@
 
 	</div>
 
-	<footer>
-		<div class="footer-info-container">
-			<p class="info-text">Nestled in the heart of Colombo, The Gallery Café has a rich history that blends culinary excellence with artistic charm. The café was founded in the early 2000s, dreamed of creating a unique dining experience that showcases traditional Sri Lankan cuisine.</p>
-
-			<div>
-				<img src="public/images/logo.png" alt="logo" />
-				<p class="copyright-text">© 2024 The Gallery Café. All rights reserved.</p>
-			</div>
-		</div>
-		<div class="footer-links-container">
-			<div class="link-block">
-				<h4>Support</h4>
-				<ul>
-					<li><a href="#">FAQs</a></li>
-					<li><a href="#">Contact Us</a></li>
-					<li><a href="#">Locate</a></li>
-					<li><a href="#">Accessibility</a></li>
-				</ul>
-			</div>
-			<div class="link-block">
-				<h4>Support</h4>
-				<ul>
-					<li><a href="#">FAQs</a></li>
-					<li><a href="#">Contact Us</a></li>
-					<li><a href="#">Locate</a></li>
-					<li><a href="#">Accessibility</a></li>
-				</ul>
-			</div>
-			<div class="link-block">
-				<h4>Support</h4>
-				<li><a href="#">FAQs</a></li>
-				<li><a href="#">Contact Us</a></li>
-				<li><a href="#">Locate</a></li>
-				<li><a href="#">Accessibility</a></li>
-				</ul>
-			</div>
-			<div class="link-block">
-				<h4>Support</h4>
-				<ul>
-					<li><a href="#">FAQs</a></li>
-					<li><a href="#">Contact Us</a></li>
-					<li><a href="#">Locate</a></li>
-					<li><a href="#">Accessibility</a></li>
-				</ul>
-			</div>
-		</div>
-	</footer>
+	<?php include('components/footer.php'); ?>
 </body>
 
 </html>
